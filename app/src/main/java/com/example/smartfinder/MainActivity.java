fbf52a250afe386eecb373de92229f39f982c43b
wjdtkdgns777 <wjdtkdgns234@naver.com>
2020-10-06 22:37:14 +0900

    32

--- app/src/main/java/com/example/smartfinder/MainActivity.java
+++ app/src/main/java/com/example/smartfinder/MainActivity.java
@@ -25,14 +25,24 @@ import com.naver.maps.map.overlay.Overlay;
 import com.naver.maps.map.overlay.OverlayImage;
 import com.naver.maps.map.util.FusedLocationSource;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 import java.util.ArrayList;
 import java.util.List;
 
 
 
-public class MainActivity extends AppCompatActivity implements OnMapReadyCallback{
+public class MainActivity extends AppCompatActivity implements NaverMap.OnMapClickListener, Overlay.OnClickListener, OnMapReadyCallback, NaverMap.OnCameraChangeListener, NaverMap.OnCameraIdleListener{
     private static final int ACCESS_LOCATION_PERMISSION_REQUEST_CODE = 100;
     private FusedLocationSource locationSource;
+    private NaverMap naverMap;
+    private InfoWindow infoWindow;
+    private List<Marker> markerList = new ArrayList<Marker>();
+    private boolean isCameraAnimated = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,6 +60,66 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         naverMap.setLocationSource(locationSource);
         UiSettings uiSettings = naverMap.getUiSettings();
         uiSettings.setLocationButtonEnabled(true);
+
+        naverMap.addOnCameraChangeListener(this);
+        naverMap.addOnCameraIdleListener(this);
+        naverMap.setOnMapClickListener(this);
+
+        LatLng mapCenter = naverMap.getCameraPosition().target;
+        fetchStoreSale(mapCenter.latitude, mapCenter.longitude, 5000);
+    }
+    @Override
+    public void onCameraChange(int reason, boolean animated) {
+        isCameraAnimated = animated;
+    }
+    @Override
+    public void onCameraIdle() {
+        if (isCameraAnimated) {
+            LatLng mapCenter = naverMap.getCameraPosition().target;
+            fetchStoreSale(mapCenter.latitude, mapCenter.longitude, 5000);
+        }
+    }
+
+    private void fetchStoreSale(double lat, double lng, int m) {
+        Retrofit retrofit = new Retrofit.Builder().baseUrl("http://211.237.50.150:7080/openapi/5010c6384bc1bf89ca9024762721a81118a3d59e00cce037a1b09b5732169e05/json/Grid_20200713000000000605_1").addConverterFactory(GsonConverterFactory.create()).build();
+        AnsimApi ansimApi = retrofit.create(AnsimApi.class);
+        ansimApi.getStoresByGeo(lat, lng, m).enqueue(new Callback<Result>() {
+            @Override
+            public void onResponse(Call<Result> call, Response<Result> response) {
+                if (response.code() == 200) {
+                    Result result = response.body();
+                    updateMapMarkers(result);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Result> call, Throwable t) {
+
+            }
+        });
+    }
+    private void updateMapMarkers(Result result) {
+        resetMarkerList();
+        if (result.stores != null && result.stores.size() > 0) {
+            for (Store store : result.stores) {
+                Marker marker = new Marker();
+                marker.setTag(store);
+                marker.setPosition(new LatLng(store.lat, store.lng));
+                if ("plenty".equalsIgnoreCase(store.remain_stat)) {
+                    marker.setIcon(OverlayImage.fromResource(R.drawable.marker_green));
+                } else if ("some".equalsIgnoreCase(store.remain_stat)) {
+                    marker.setIcon(OverlayImage.fromResource(R.drawable.marker_yellow));
+                } else if ("fiew".equalsIgnoreCase(store.remain_stat)) {
+                    marker.setIcon(OverlayImage.fromResource(R.drawable.marker_red));
+                } else {
+                    marker.setIcon(OverlayImage.fromResource(R.drawable.marker_gray));
+                }
+                marker.setAnchor(new PointF(0.5f, 1.0f));
+                marker.setMap(naverMap);
+                marker.setOnClickListener(this);
+                markerList.add(marker);
+            }
+        }
     }
 
     @Override
@@ -62,5 +132,16 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                 return;
         }
     }
+    private void resetMarkerList() {
+        if (markerList != null && markerList.size() > 0) {
+            for (Marker marker : markerList) {
+                marker.setMap(null);
+            }
+            markerList.clear();
+        }
+    }
+
+
+
 
 }
