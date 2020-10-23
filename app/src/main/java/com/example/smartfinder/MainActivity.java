c2783c425f080993c2419c516500649b797dadb5
wjdtkdgns777 <wjdtkdgns234@naver.com>
2020-10-24 04:34:19 +0900

    1주차 네이버 맵 구현 네이버 gps 현재 위치 돌아가기 구현 깃허브 연결 구현 도움말 구현

--- app/src/main/java/com/example/smartfinder/MainActivity.java
+++ app/src/main/java/com/example/smartfinder/MainActivity.java
@@ -2,17 +2,22 @@ package com.example.smartfinder;
 
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.graphics.PointF;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.naver.maps.geometry.LatLng;
 import com.naver.maps.map.MapFragment;
@@ -25,24 +30,16 @@ import com.naver.maps.map.overlay.Overlay;
 import com.naver.maps.map.overlay.OverlayImage;
 import com.naver.maps.map.util.FusedLocationSource;
 
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
-
 import java.util.ArrayList;
 import java.util.List;
 
 
 
-public class MainActivity extends AppCompatActivity implements NaverMap.OnMapClickListener, Overlay.OnClickListener, OnMapReadyCallback, NaverMap.OnCameraChangeListener, NaverMap.OnCameraIdleListener{
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback{
     private static final int ACCESS_LOCATION_PERMISSION_REQUEST_CODE = 100;
     private FusedLocationSource locationSource;
-    private NaverMap naverMap;
-    private InfoWindow infoWindow;
-    private List<Marker> markerList = new ArrayList<Marker>();
-    private boolean isCameraAnimated = false;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,7 +48,65 @@ public class MainActivity extends AppCompatActivity implements NaverMap.OnMapCli
 
         MapFragment mapFragment = (MapFragment)getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+
+        ImageView imageView = (ImageView)findViewById(R.id.img);
+
+
+        imageView.setVisibility(View.INVISIBLE);
+
+
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(myToolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_baseline_dehaze_24);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //return super.onCreateOptionsMenu(menu);
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        //return super.onOptionsItemSelected(item);
+        switch (item.getItemId()) {
+            case R.id.action_settings1:
+
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/wjdtkdgns777/smartfinder")); startActivity(intent);
+
+                return super.onOptionsItemSelected(item);
+
+            case R.id.action_settings2:
+
+
+                ImageView img =(ImageView)findViewById(R.id.img);
+                if(img.getVisibility()==View.VISIBLE){
+                    img.setVisibility(View.INVISIBLE);
+                }else {
+                    img.setVisibility(View.VISIBLE);
+                }
+
+
+                return super.onOptionsItemSelected(item);
+
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                Toast.makeText(getApplicationContext(), "나머지 버튼 클릭됨", Toast.LENGTH_LONG).show();
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
+
+
+
     @Override
     public void onMapReady(@NonNull NaverMap naverMap) {
 
@@ -60,66 +115,6 @@ public class MainActivity extends AppCompatActivity implements NaverMap.OnMapCli
         naverMap.setLocationSource(locationSource);
         UiSettings uiSettings = naverMap.getUiSettings();
         uiSettings.setLocationButtonEnabled(true);
-
-        naverMap.addOnCameraChangeListener(this);
-        naverMap.addOnCameraIdleListener(this);
-        naverMap.setOnMapClickListener(this);
-
-        LatLng mapCenter = naverMap.getCameraPosition().target;
-        fetchStoreSale(mapCenter.latitude, mapCenter.longitude, 5000);
-    }
-    @Override
-    public void onCameraChange(int reason, boolean animated) {
-        isCameraAnimated = animated;
-    }
-    @Override
-    public void onCameraIdle() {
-        if (isCameraAnimated) {
-            LatLng mapCenter = naverMap.getCameraPosition().target;
-            fetchStoreSale(mapCenter.latitude, mapCenter.longitude, 5000);
-        }
-    }
-
-    private void fetchStoreSale(double lat, double lng, int m) {
-        Retrofit retrofit = new Retrofit.Builder().baseUrl("http://211.237.50.150:7080/openapi/5010c6384bc1bf89ca9024762721a81118a3d59e00cce037a1b09b5732169e05/json/Grid_20200713000000000605_1").addConverterFactory(GsonConverterFactory.create()).build();
-        AnsimApi ansimApi = retrofit.create(AnsimApi.class);
-        ansimApi.getStoresByGeo(lat, lng, m).enqueue(new Callback<Result>() {
-            @Override
-            public void onResponse(Call<Result> call, Response<Result> response) {
-                if (response.code() == 200) {
-                    Result result = response.body();
-                    updateMapMarkers(result);
-                }
-            }
-
-            @Override
-            public void onFailure(Call<Result> call, Throwable t) {
-
-            }
-        });
-    }
-    private void updateMapMarkers(Result result) {
-        resetMarkerList();
-        if (result.stores != null && result.stores.size() > 0) {
-            for (Store store : result.stores) {
-                Marker marker = new Marker();
-                marker.setTag(store);
-                marker.setPosition(new LatLng(store.lat, store.lng));
-                if ("plenty".equalsIgnoreCase(store.remain_stat)) {
-                    marker.setIcon(OverlayImage.fromResource(R.drawable.marker_green));
-                } else if ("some".equalsIgnoreCase(store.remain_stat)) {
-                    marker.setIcon(OverlayImage.fromResource(R.drawable.marker_yellow));
-                } else if ("fiew".equalsIgnoreCase(store.remain_stat)) {
-                    marker.setIcon(OverlayImage.fromResource(R.drawable.marker_red));
-                } else {
-                    marker.setIcon(OverlayImage.fromResource(R.drawable.marker_gray));
-                }
-                marker.setAnchor(new PointF(0.5f, 1.0f));
-                marker.setMap(naverMap);
-                marker.setOnClickListener(this);
-                markerList.add(marker);
-            }
-        }
     }
 
     @Override
@@ -132,16 +127,13 @@ public class MainActivity extends AppCompatActivity implements NaverMap.OnMapCli
                 return;
         }
     }
-    private void resetMarkerList() {
-        if (markerList != null && markerList.size() > 0) {
-            for (Marker marker : markerList) {
-                marker.setMap(null);
-            }
-            markerList.clear();
-        }
-    }
-
-
-
 
 }
+
+
+
+
+
+
+
+
